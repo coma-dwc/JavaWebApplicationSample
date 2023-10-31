806de1489ed58c7a793ecbfd566200d5b3436eff
coma-dwc <tamagomapon@gmail.com>
2023-10-31 12:17:51 +0900

    終了

---
+++ src/main/java/sample/HelloServlet.java
@@ -0,0 +1,51 @@
+package sample;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloServlet
+ */
+@WebServlet("/HelloServlet")
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		// request.jspから送られてきたテキストを受け取りtextという変数に要れる
+		String text = request.getParameter("textName");
+		// 送られてきたテキストにworld!を追加する
+		text += "world!";
+		// response.jspで文字列を取得するための準備
+		request.setAttribute("afterServlet",  text);
+		
+		// request.jspを表示する
+		request.getRequestDispatcher("response.jsp").forward(request, response);
+		//response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
